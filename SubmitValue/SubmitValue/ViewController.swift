b2747ab45e68f3f2180038581d153873964f9220
haroldjung <devangel83@gmail.com>
2018-07-09 07:35:53 +0900

    home0709

--- SubmitValue/SubmitValue/ViewController.swift
+++ SubmitValue/SubmitValue/ViewController.swift
@@ -10,16 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    //이메일 주소를 입력받는 텍스트필드
+    @IBOutlet weak var email: UITextField!
+    
+    //자동 갱신 여부를 설정하는 스위치
+    @IBOutlet weak var isUpdate: UISwitch!
+    
+    
+    //갱신 주기를 설정하는 스테퍼
+    @IBOutlet weak var interval: UIStepper!
+    
+    //자동갱신 여부를 표시하는 레이블
+    @IBOutlet weak var isUpdateText: UILabel!
+ 
+    //갱신주기를 텍스트로 표시하는 레이블
+    @IBOutlet weak var intervalText: UILabel!
+    
+    
 }
 
