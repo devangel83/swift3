2233848ee542d14b12beac61bba44dcf6488cd94
haroldjung <devangel83@gmail.com>
2018-07-25 08:10:37 +0900

    home0725

--- MyMoveChart/MyMoveChart/ListViewController.swift
+++ MyMoveChart/MyMoveChart/ListViewController.swift
@@ -1,5 +1,36 @@
 import UIKit
 
 class ListViewController : UITableViewController {
+    //튜플 아이템으로 구성된 데이터 세트
+    var dataset = [
+        ("다크나이트", "영웅물에 철학에 음악까지 더해져 예술이 되다.","2008-09-04",8.95),
+        ("호우시절", "때를 알고 내리는 좋은 비","2009-10-14",7.33),
+        ("말할 수 없는 비밀", "여기서 너까지 다섯 걸음","2015-05-07",9.95)
+    ]
+    
+    lazy var list : [MovieVO] = {
+        var datalist = [MovieVO]()
+        
+        for (title, desc, opendate, rating) in self.dataset {
+            let mvo = MovieVO()
+            
+            mvo.title = title
+            mvo.description = desc
+            mvo.opendate = opendate
+            mvo.rating = rating
+            
+            datalist.append(mvo)
+        }
+        
+        return datalist
+    }()
+
+    
+    override func viewDidLoad() {
+        
+        
+    }
+
+    
     
 }
---
+++ MyMoveChart/MyMoveChart/MovieVO.swift
@@ -0,0 +1,20 @@
+//
+//  MovieVO.swift
+//  MyMoveChart
+//
+//  Created by HaroldJung on 2018. 7. 25..
+//  Copyright © 2018년 moonth. All rights reserved.
+//
+
+import Foundation
+
+class MovieVO {
+    var thumbnail : String? //영화 썸네일 이미지 주소
+    var title : String? //제목
+    var description : String? //설명
+    var detail : String? //상세정보
+    var opendate : String? //개봉일
+    var rating : Double? //평점
+    
+    
+}
